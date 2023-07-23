8e36e560cec61e14655d2f5a9aebe6f767b0e0a8
Yulian <khantermail@gmail.com>
2023-07-23 17:34:19 +0300

    adding format arg

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,11 @@
 """Main script and entry point"""
 import argparse
 
+FORMAT = ['JSON', 'plain']
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')  # noqa: E501
 parser.add_argument('first_file', type=str)
 parser.add_argument('second_file', type=str)
+parser.add_argument('-f', '--format', default=FORMAT, help='set format of output')
 args = parser.parse_args()
 
 
